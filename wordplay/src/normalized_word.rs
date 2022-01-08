c0a2846c0ad7454c6cd5e6c82a64854a4f997cfa
Alex Gill <alex.gill@depop.com>
2022-01-08 12:45:45 +0000

    Normalize accented letters

--- wordplay/src/normalized_word.rs
+++ wordplay/src/normalized_word.rs
@@ -39,7 +39,21 @@ impl NormalizedChar {
             return num::FromPrimitive::from_u8(u8_ch);
         }
 
-        None
+        use NormalizedChar::*;
+
+        let nc = match ch {
+            'á' | 'Á' | 'â' | 'Â' | 'ä' | 'Ä' | 'à' | 'À' | 'ã' | 'Ã' | 'å' | 'Å' => A,
+            'ç' | 'Ç' => C,
+            'é' | 'É' | 'ê' | 'Ê' | 'ë' | 'Ë' | 'è' | 'È' => E,
+            'í' | 'Í' | 'î' | 'Î' | 'ï' | 'Ï' | 'ì' | 'Ì' => I,
+            'ñ' | 'Ñ' => N,
+            'ó' | 'Ó' | 'ô' | 'Ô' | 'ö' | 'Ö' | 'ò' | 'Ò' | 'õ' | 'Õ' => O,
+            'ú' | 'Ú' | 'û' | 'Û' | 'ü' | 'Ü' | 'ù' | 'Ù' => U,
+            'ý' | 'Ý' => Y,
+            _ => return None,
+        };
+
+        Some(nc)
     }
 }
 
@@ -88,7 +102,7 @@ mod tests {
     use NormalizedChar::*;
 
     #[test]
-    fn normalized_word_creates_from_ascii_uppercase() {
+    fn creates_from_ascii_uppercase() {
         let nw = NormalizedWord::from_str("ABC");
 
         let expected = NormalizedWord::new(vec![A, B, C]);
@@ -97,7 +111,7 @@ mod tests {
     }
 
     #[test]
-    fn normalized_word_creates_from_ascii_lowercase() {
+    fn creates_from_ascii_lowercase() {
         let nw = NormalizedWord::from_str("abc");
 
         let expected = NormalizedWord::new(vec![A, B, C]);
@@ -106,12 +120,33 @@ mod tests {
     }
 
     #[test]
-    fn normalized_word_ignores_non_letters() {
+    fn ignores_non_letters() {
         let nw = NormalizedWord::from_str("A1B2C3");
 
         let expected = NormalizedWord::new(vec![A, B, C]);
 
         assert_eq!(nw, expected);
+    }
+
+    #[test]
+    fn marshalls_accented_letters() {
+        [
+            ("áÁâÂäÄàÀãÃåÅ", "AAAAAAAAAAAA"),
+            ("çÇ", "CC"),
+            ("éÉêÊëËèÈ", "EEEEEEEE"),
+            ("íÍîÎïÏìÌ", "IIIIIIII"),
+            ("ñÑ", "NN"),
+            ("óÓôÔöÖòÒõÕ", "OOOOOOOOOO"),
+            ("úÚûÛüÜùÙ", "UUUUUUUU"),
+            ("ýÝ", "YY"),
+        ]
+        .iter()
+        .for_each(|(str, expected)| {
+            assert_eq!(
+                NormalizedWord::from_str(str),
+                NormalizedWord::from_str(expected)
+            )
+        })
     }
 
     fn mk(str: &str) -> NormalizedWord {

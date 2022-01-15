4d627bfb845ea7894f1268daaa52de1941711d95
Alex Gill <alex.gill@depop.com>
2022-01-15 20:37:12 +0000

    Fix benches

--- wordplay/benches/wordplay_bench.rs
+++ wordplay/benches/wordplay_bench.rs
@@ -1,7 +1,10 @@
 use std::fs::File;
 
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use wordplay::{dictionary::Dictionary, normalized_word::NormalizedWord, trie::TrieSearch};
+use wordplay::{
+    dictionary::{DictSearch, Dictionary},
+    normalized_word::NormalizedWord,
+};
 
 fn enable_bench(c: &mut Criterion) {
     let enable = Dictionary::from_file(File::open("data/enable.txt").unwrap());
@@ -14,7 +17,7 @@ fn enable_bench(c: &mut Criterion) {
     c.bench_function("enable search bana??", |b| {
         b.iter(|| {
             enable
-                .iter_search(black_box(TrieSearch::exactly("bana??")))
+                .iter_search(black_box(DictSearch::from_pattern("bana??")))
                 .count()
         })
     });
@@ -22,7 +25,7 @@ fn enable_bench(c: &mut Criterion) {
     c.bench_function("enable search ban prefix", |b| {
         b.iter(|| {
             enable
-                .iter_search(black_box(TrieSearch::from_prefix("ban")))
+                .iter_search(black_box(DictSearch::from_pattern("ban")))
                 .count()
         })
     });
@@ -30,7 +33,7 @@ fn enable_bench(c: &mut Criterion) {
     c.bench_function("enable search ?an prefix", |b| {
         b.iter(|| {
             enable
-                .iter_search(black_box(TrieSearch::from_prefix("?an")))
+                .iter_search(black_box(DictSearch::from_pattern("?an")))
                 .count()
         })
     });

3c73e92138f950297aa967a8b6c55e3f8e32831a
Alex Gill <alex.gill@depop.com>
2022-01-15 11:30:55 +0000

    Refactor ENABLE tests

--- wordplay/tests/integration_test.rs
+++ wordplay/tests/integration_test.rs
@@ -4,31 +4,36 @@ use std::fs::File;
 
 use wordplay::dictionary::{DictSearch, Dictionary};
 
-lazy_static! {
-    static ref DICT: Dictionary = {
-        let file = File::open("data/enable.txt").unwrap();
-        Dictionary::from_file(file)
-    };
-}
+#[cfg(test)]
+mod enable_tests {
+    use super::*;
 
-#[test]
-fn file_loads_all_lines() {
-    let dict_size = DICT.iter().count();
-    assert_eq!(dict_size, 172820)
-}
+    lazy_static! {
+        static ref ENABLE: Dictionary = {
+            let file = File::open("data/enable.txt").unwrap();
+            Dictionary::from_file(file)
+        };
+    }
 
-#[test]
-fn find_matching_words() {
-    let search = DictSearch::from_pattern("?ana");
+    #[test]
+    fn file_loads_all_lines() {
+        let dict_size = ENABLE.iter().count();
+        assert_eq!(dict_size, 172820)
+    }
 
-    let mut iter = DICT.iter_search(search).map(|x| &x.original[..]);
+    #[test]
+    fn find_matching_words() {
+        let search = DictSearch::from_pattern("?ana");
 
-    assert!(iter.any(|x| x == "banana"));
-}
+        let mut iter = ENABLE.iter_search(search).map(|x| &x.original[..]);
 
-#[test]
-fn all_words_have_anagram_num() {
-    for x in DICT.iter() {
-        assert!(x.anag_num.is_some());
+        assert!(iter.any(|x| x == "banana"));
+    }
+
+    #[test]
+    fn all_words_have_anagram_num() {
+        for x in ENABLE.iter() {
+            assert!(x.anag_num.is_some());
+        }
     }
 }
